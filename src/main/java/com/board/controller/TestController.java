d685096ced39f38b198c125c8eaa533591d3326a
SLCube <kgm0096@naver.com>
2021-12-02 14:50:53 +0900

    댓글 CRUD구현

---
+++ src/main/java/com/board/controller/TestController.java
@@ -0,0 +1,33 @@
+package com.board.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping(value = "/members")
+	@ResponseBody	// public @ResponseBody String testByResponseBody()와 같이 리턴 타입 좌측에 지정 가능
+	public Map<Integer, Object> testByResponseBody() {
+		
+		Map<Integer, Object> members = new HashMap<Integer, Object>();
+		
+		for(int i = 1; i <= 20; i++) {
+			Map<String, Object> member = new HashMap<String, Object>();
+			member.put("idx", i);
+			member.put("nickname", i + "길동");
+			member.put("height", i + 20);
+			member.put("weight", i + 30);
+			members.put(i, member);
+		}
+		
+		return members;
+		
+	}
+	
+	
+}
