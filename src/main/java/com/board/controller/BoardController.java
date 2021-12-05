606b8c03441acddb24ec1dd5bccfb536bd31586d
SLCube <kgm0096@naver.com>
2021-12-05 21:29:55 +0900

    no message

--- src/main/java/com/board/controller/BoardController.java
+++ src/main/java/com/board/controller/BoardController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.board.constant.Method;
 import com.board.domain.BoardDTO;
@@ -41,10 +40,10 @@ public class BoardController extends UiUtils {
 	}
 
 	@PostMapping(value = "/board/register.do")
-	public String registerBoard(final BoardDTO params, final MultipartFile[] files, Model model) {
+	public String registerBoard(@ModelAttribute("params") BoardDTO params, Model model) {
 		Map<String, Object> pagingParams = getPagingParams(params);
 		try {
-			boolean isRegistered = boardService.registerBoard(params, files);
+			boolean isRegistered = boardService.registerBoard(params);
 			if (isRegistered == false) {
 				return showMessageWithRedirect("게시글 등록에 실패하였습니다.", "/board/list.do", Method.GET, pagingParams, model);
 			}
@@ -54,7 +53,6 @@ public class BoardController extends UiUtils {
 		} catch (Exception e) {
 			return showMessageWithRedirect("시스템에 문제가 발생하였습니다.", "/board/list.do", Method.GET, pagingParams, model);
 		}
-
 		return showMessageWithRedirect("게시글 등록이 완료되었습니다.", "/board/list.do", Method.GET, pagingParams, model);
 	}
 
--- src/main/java/com/board/service/BoardServiceImpl.java
+++ src/main/java/com/board/service/BoardServiceImpl.java
@@ -3,18 +3,14 @@ package com.board.service;
 import java.util.Collections;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.CollectionUtils;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.board.domain.AttachDTO;
 import com.board.domain.BoardDTO;
-import com.board.mapper.AttachMapper;
 import com.board.mapper.BoardMapper;
 import com.board.paging.PaginationInfo;
-import com.board.util.FileUtils;
 
 @Service
 @Transactional
@@ -22,13 +18,7 @@ public class BoardServiceImpl implements BoardService {
 
 	@Autowired
 	private BoardMapper boardMapper;
-
-	@Autowired
-	private AttachMapper attachMapper;
-
-	@Autowired
-	private FileUtils fileUtils;
-
+	
 	@Override
 	public boolean registerBoard(BoardDTO params) {
 		int queryResult = 0;
@@ -38,27 +28,8 @@ public class BoardServiceImpl implements BoardService {
 		} else {
 			queryResult = boardMapper.updateBoard(params);
 		}
-
+		
 		return (queryResult == 1) ? true : false;
-	}
-
-	@Override
-	public boolean registerBoard(BoardDTO params, MultipartFile[] files) {
-		int queryResult = 1;
-
-		if (registerBoard(params) == false) {
-			return false;
-		}
-
-		List<AttachDTO> fileList = fileUtils.uploadFiles(files, params.getIdx());
-		if (CollectionUtils.isEmpty(fileList) == false) {
-			queryResult = attachMapper.insertAttach(fileList);
-			if (queryResult < 1) {
-				queryResult = 0;
-			}
-		}
-
-		return (queryResult > 0);
 	}
 
 	@Override
@@ -69,13 +40,13 @@ public class BoardServiceImpl implements BoardService {
 	@Override
 	public boolean deleteBoard(Long idx) {
 		int queryResult = 0;
-
+		
 		BoardDTO board = boardMapper.selectBoardDetail(idx);
-
-		if (board != null && "N".equals(board.getDeleteYn())) {
+		
+		if(board != null && "N".equals(board.getDeleteYn())) {
 			queryResult = boardMapper.deleteBoard(idx);
 		}
-
+		
 		return (queryResult == 1) ? true : false;
 	}
 
